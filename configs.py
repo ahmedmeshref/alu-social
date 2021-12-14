ac3ccd8a4a23d87f261e896aa8271d81fedc61af
ahmedmeshref <a.meshref@alustudent.com>
2021-12-14 02:53:13 +0200

    dd chancges

--- configs.py
+++ configs.py
@@ -4,7 +4,9 @@ import os
 class Config(object):
     SECRET_KEY = os.urandom(32)
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("POSTGRESQL") + 'alu_social?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres@alusocial:123456Ahmed@alusocial.postgres.database.azure.com:5432/"  + 'alu_social?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_ECHO = True
     
+
+
